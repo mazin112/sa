586a2abe620c4c4488d62358bb5e5259aa5aa683
mazin112 <120086917+mazin112@users.noreply.github.com>
2023-07-01 20:59:27 +0200

    Update s1.py

--- s1.py
+++ s1.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 import math
 import time
 import base64
+from urllib.parse import urlparse, unquote
+import mimetypes
 
 # Telegram bot token
 TOKEN = '6235045070:AAF_tTnNTDYBrNQhO7rkBuDpF6fcl21Ilss'
@@ -24,11 +26,11 @@ def start_command(message):
     chat_id = message.chat.id
     if chat_id not in users:
         users[chat_id] = {'state': 'email'}
-        bot.reply_to(message, f"Welcome to the MEGA Uploader bot, {message.from_user.first_name}!\n\n"
-                              "To get started, please use the /login command to log in.")
+        bot.send_message(chat_id, f"Welcome to the MEGA Uploader bot, {message.from_user.first_name}!\n\n"
+                                  "To get started, please use the /login command to log in.")
     else:
-        bot.reply_to(message, f"You are already registered, {message.from_user.first_name}! "
-                              "Please use the /login command to log in.")
+        bot.send_message(chat_id, f"You are already registered, {message.from_user.first_name}! "
+                                  "Please use the /login command to log in.")
 
 
 @bot.message_handler(commands=['login'])
@@ -38,11 +40,11 @@ def login_command(message):
         user = users[chat_id]
         if 'email' not in user:
             user['state'] = 'email'
-            bot.reply_to(message, "Please enter your MEGA email.")
+            bot.send_message(chat_id, "Please enter your MEGA email.")
         else:
-            bot.reply_to(message, "You are already logged in. Use the /upload command to upload files.")
+            bot.send_message(chat_id, "You are already logged in. Use the /upload command to upload files.")
     else:
-        bot.reply_to(message, "You need to start the bot first with the /start command.")
+        bot.send_message(chat_id, "You need to start the bot first with the /start command.")
 
 
 @bot.message_handler(func=lambda message: True)
@@ -54,22 +56,64 @@ def handle_message(message):
         # Save the email
         user['email'] = message.text.strip()
         user['state'] = 'password'
-        bot.reply_to(message, "Email set: {}".format(user['email']))
-        bot.reply_to(message, "Please enter your MEGA password.")
+        bot.send_message(chat_id, "Email set: {}".format(user['email']))
+        bot.send_message(chat_id, "Please enter your MEGA password.")
     elif user['state'] == 'password':
         # Save the password
         user['password'] = message.text.strip()
         user['state'] = 'links'
-        bot.reply_to(message, "Password set: {}".format('*' * len(user['password'])))
-        bot.reply_to(message, "Please enter the direct links of the files (comma-separated).")
+        bot.send_message(chat_id, "Password set: {}".format('*' * len(user['password'])))
+        bot.send_message(chat_id, "Please enter the direct links of the files (comma-separated).")
     elif user['state'] == 'links':
         # Process the direct links and upload files
         links = message.text.strip().split(',')
         mega_instance = login_mega(user['email'], user['password'])
         if mega_instance is None:
-            bot.reply_to(message, "MEGA login failed.")
+            bot.send_message(chat_id, "MEGA login failed.")
+            return
+
+        # Create a reply keyboard markup
+        reply_markup = ReplyKeyboardMarkup(resize_keyboard=True)
+
+        # Check if a task is already in progress
+        if 'current_task' in user and user['current_task'] is not None:
+            # Put the new task on hold
+            user['on_hold_task'] = links
+            bot.send_message(chat_id, "Processing...ðŸ¤Œ\n"
+                                      "The current task is in progress. The new task has been put on hold.\n"
+                                      "Number of tasks on hold: {}".format(len(user['on_hold_task'])))
         else:
-            upload_files(chat_id, mega_instance, links, message)  # Pass the 'message' parameter
+            # Start the new task
+            user['current_task'] = links
+            process_task(chat_id, mega_instance)
+
+
+@bot.message_handler(commands=['cancel'])
+def cancel_command(message):
+    chat_id = message.chat.id
+    user = users.get(chat_id)
+    if user and 'current_task' in user:
+        user['cancel'] = True
+        if 'on_hold_task' in user:
+            user['current_task'] = user['on_hold_task']
+            del user['on_hold_task']
+            process_task(chat_id)
+        else:
+            bot.send_message(chat_id, "Task cancellation requested.")
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "cancel_task")
+def handle_callback_query(call):
+    chat_id = call.message.chat.id
+    user = users.get(chat_id)
+    if user and 'current_task' in user:
+        user['cancel'] = True
+        if 'on_hold_task' in user:
+            user['current_task'] = user['on_hold_task']
+            del user['on_hold_task']
+            process_task(chat_id)
+        else:
+            bot.answer_callback_query(call.id, text="Task cancellation requested.")
 
 
 def login_mega(email, password):
@@ -83,71 +127,13 @@ def login_mega(email, password):
         return None
 
 
-def upload_files(chat_id, mega, links, message):  # Add 'message' parameter
-    for link in links:
-        link = link.strip()
-
-        # Download the file from the direct link
-        response = requests.get(link, stream=True)
-        file_name = link.split('/')[-1]
-        file_size = response.headers.get('Content-Length')
-
-        # Download the file and display progress
-        temp_file_path = os.path.join(tempfile.gettempdir(), file_name)
-        total_size = int(file_size)
-        chunk_size = 1024 * 1024  # 1 MB chunk size
-
-        progress_message = bot.reply_to(message, "Downloading file: {}".format(file_name))
-
-        with tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as progress_bar:
-            start_time = time.time()
-
-            with open(temp_file_path, 'wb') as file:
-                for chunk in response.iter_content(chunk_size=chunk_size):
-                    if chunk:
-                        file.write(chunk)
-                        progress_bar.update(len(chunk))
-                        current_time = time.time()
-                        elapsed_time = current_time - start_time
-
-                        # Update progress every 5 seconds
-                        if elapsed_time > 5:
-                            update_progress(chat_id, progress_message.message_id, progress_bar, total_size)
-                            start_time = current_time
-
-            progress_bar.close()
-
-        # Remove the progress message for download
-        bot.delete_message(chat_id, progress_message.message_id)
-
-        # Save upload progress info
-        if 'upload_info' not in users[chat_id]:
-            users[chat_id]['upload_info'] = {}
-        users[chat_id]['upload_info'][file_name] = {'total_size': total_size, 'progress_bar': progress_bar}
-
-        # Send progress message for upload
-        upload_progress_message = bot.reply_to(message, "Uploading file to MEGA: {}".format(file_name))
-
-        # Upload the file to MEGA
-        uploaded_file = upload_file(mega, temp_file_path, file_name)
-
-        if uploaded_file is not None:
-            # Get the download link of the uploaded file
-            download_url = mega.get_upload_link(uploaded_file)
-            bot.reply_to(message, "File uploaded to MEGA: {}\n\n"
-                                  "Download link: {}".format(file_name, download_url))
-        else:
-            bot.reply_to(message, "Failed to upload file to MEGA: {}".format(file_name))
-
-        # Remove the temporary file
-        os.remove(temp_file_path)
-
-        # Remove the progress message for upload
-        bot.delete_message(chat_id, upload_progress_message.message_id)
-
-
 def upload_file(mega, file_path, file_name):
     try:
+        # Decode the file name if it's Base64 encoded
+        if "?" in file_name:
+            file_name = file_name.split("?")[0]
+            file_name = base64.urlsafe_b64decode(file_name).decode()
+
         uploaded_file = mega.find(file_name)
         if uploaded_file:
             print("File already exists in MEGA:", file_name)
@@ -175,30 +161,164 @@ def update_progress(chat_id, message_id, progress_bar, total_size):
     if "remaining" in progress_bar.format_dict:
         remaining = progress_bar.format_dict["remaining"]
         progress_text = f"Downloading file:\n\n" \
-                        f"[{'○' * (10 - (percent // 10))}{'●' * (percent // 10)}]\n" \
+                        f"[{'â—‹' * (10 - (percent // 10))}{'â—' * (percent // 10)}]\n" \
                         f"{format_size(progress_bar.format_dict['n'])} of {total_size_formatted}\n" \
                         f"Speed: {speed_formatted}\n" \
                         f"ETA: {remaining}"
     else:
         progress_text = f"Downloading file:\n\n" \
-                        f"[{'○' * (10 - (percent // 10))}{'●' * (percent // 10)}]\n" \
+                        f"[{'â—‹' * (10 - (percent // 10))}{'â—' * (percent // 10)}]\n" \
                         f"{format_size(progress_bar.format_dict['n'])} of {total_size_formatted}\n" \
                         f"Speed: {speed_formatted}"
 
-    # Update the progress message with a fancy progress bar
-    progress_text += f"\n\nProgress: {'█' * (percent // 10)}{' ' * ((100 - percent) // 10)} {percent}%"
-
-    bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=progress_text)
+    cancel_button = InlineKeyboardButton("Cancel Task", callback_data="cancel_task")
+    inline_keyboard = InlineKeyboardMarkup().add(cancel_button)
+    bot.edit_message_text(chat_id=chat_id, message_id=message_id, text=progress_text,
+                          reply_markup=inline_keyboard)
 
 
 def format_size(size):
-    power = 2 ** 10
-    n = 0
-    power_labels = {0: '', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
-    while size > power:
-        size /= power
-        n += 1
-    return str(round(size, 2)) + ' ' + power_labels[n] + 'B'
+    units = ["B", "KB", "MB", "GB", "TB"]
+    index = 0
+    while size >= 1024 and index < len(units) - 1:
+        size /= 1024
+        index += 1
+    size_formatted = "{:.2f} {}".format(size, units[index])
+    return size_formatted
+
+
+def process_task(chat_id, mega_instance):
+    user = users.get(chat_id)
+    if not user:
+        return
+
+    links = user['current_task']
+
+    # Upload the files
+    for link in links:
+        link = link.strip()
+
+        # Download the file from the direct link
+        response = requests.get(link, stream=True)
+        file_name = link.split('/')[-1]
+        file_size = response.headers.get('Content-Length')
+
+        # Check if the file is too large to upload
+        max_file_size = 10000 * 1024 * 1024  # Set maximum file size to 10000 MB
+        if file_size is not None and int(file_size) > max_file_size:
+            bot.send_message(chat_id, "File is too large to upload: {}".format(file_name))
+            continue
+
+        # Download the file and display progress
+        temp_file_path = os.path.join(tempfile.gettempdir(), file_name)
+        with open(temp_file_path, 'wb') as file:
+            total_size = int(file_size) if file_size is not None else 0
+            chunk_size = 1024 * 1024  # 1 MB chunk size
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, ncols=50,
+                                bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]')
+
+            progress_message = bot.send_message(chat_id, f"Downloading file:\n\n"
+                                                         f"[{'â–«' * 10}]\n"
+                                                         f"0B of {format_size(total_size)}",
+                                                reply_markup=InlineKeyboardMarkup().add(
+                                                    InlineKeyboardButton("Cancel Task", callback_data="cancel_task")))
+
+            start_time = time.time()
+            for chunk in response.iter_content(chunk_size):
+                if 'cancel' in user and user['cancel']:
+                    # Remove the cancel flag and stop the task
+                    user['cancel'] = False
+                    progress_bar.close()
+                    bot.edit_message_text(chat_id=chat_id, message_id=progress_message.message_id,
+                                          text="Task canceled.")
+                    return
+
+                file.write(chunk)
+                progress_bar.update(len(chunk))
+                current_time = time.time()
+                elapsed_time = current_time - start_time
+
+                # Update progress every 5 seconds
+                if elapsed_time > 5:
+                    update_progress(chat_id, progress_message.message_id, progress_bar, total_size)
+                    start_time = current_time
+
+            progress_bar.close()
+
+        # Check the file type
+        file_type = get_file_type(temp_file_path)
+
+        if file_type == 'video':
+            # Upload the video file to MEGA
+            uploaded_file = upload_file(mega_instance, temp_file_path, file_name)
+
+            if uploaded_file is not None:
+                # Get the download link of the uploaded file
+                download_url = mega_instance.get_upload_link(uploaded_file)
+                short_name = get_short_name(file_name)
+                bot.send_message(chat_id, "Video file uploaded to MEGA: {}\n\n"
+                                          "Download link: {}".format(short_name, download_url))
+            else:
+                bot.send_message(chat_id, "Failed to upload video file to MEGA: {}".format(file_name))
+
+        elif file_type == 'application':
+            # Check if the file already exists in MEGA
+            if mega_instance.find(temp_file_path):
+                bot.send_message(chat_id, "File already exists in MEGA: {}".format(file_name))
+                os.remove(temp_file_path)
+                continue
+
+            # Upload the application file to MEGA
+            uploaded_file = upload_file(mega_instance, temp_file_path, file_name)
+
+            if uploaded_file is not None:
+                # Get the download link of the uploaded file
+                download_url = mega_instance.get_upload_link(uploaded_file)
+                short_name = get_short_name(file_name)
+                bot.send_message(chat_id, "Application file uploaded to MEGA: {}\n\n"
+                                          "Download link: {}".format(short_name, download_url))
+            else:
+                bot.send_message(chat_id, "Failed to upload application file to MEGA: {}".format(file_name))
+
+        else:
+            bot.send_message(chat_id, "Unsupported file type: {}".format(file_name))
+
+        # Remove the temporary file
+        os.remove(temp_file_path)
+
+    # Remove the current task from the user data
+    del user['current_task']
+
+    # Check if there is an on-hold task
+    if 'on_hold_task' in user:
+        on_hold_task = user['on_hold_task']
+        del user['on_hold_task']
+        user['current_task'] = on_hold_task
+        process_task(chat_id, mega_instance)
+    else:
+        bot.send_message(chat_id, "Task completed.")
+
+
+
+def get_file_type(file_path):
+    mime_type, _ = mimetypes.guess_type(file_path)
+    if mime_type is None:
+        return None
+
+    if mime_type.startswith('video'):
+        return 'video'
+    elif mime_type.startswith('application'):
+        return 'application'
+
+    return None
+
+
+def get_short_name(file_name):
+    if len(file_name) > 30:
+        short_name = file_name[:20] + "..." + file_name[-7:]
+    else:
+        short_name = file_name
+    return short_name
 
 
 bot.polling()
